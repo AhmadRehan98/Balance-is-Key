524ac6df305cef4c7dad0e642a63aced579413da
Kevin Covelli <kevin.covelli@mail.utoronto.ca>
2021-09-25 13:50:13 -0400

    multiplayer input and cat model

--- Balance is Key GameJam 2/Assets/Scripts/move.cs
+++ Balance is Key GameJam 2/Assets/Scripts/move.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     private float _userVerticalInput, _userHorizontalInput;
+    public string player;
 
     private float speed = 0.01f;
     // Start is called before the first frame update
@@ -16,8 +17,8 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _userVerticalInput = Input.GetAxis("Vertical");
-        _userHorizontalInput = Input.GetAxis("Horizontal");
+        _userVerticalInput = Input.GetAxis(player + "Vertical");
+        _userHorizontalInput = Input.GetAxis(player + "Horizontal");
         Debug.Log(_userVerticalInput);
         gameObject.GetComponent<Transform>().position += transform.forward * _userVerticalInput * speed;
         gameObject.GetComponent<Transform>().position += transform.right * _userHorizontalInput * speed;
