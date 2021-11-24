bbe8d4128ab60ad35fdf62b733b140a4e2acd600
Ahmad Rehan <a.rehan@mail.utoronto.ca>
2021-11-23 23:47:56 -0500

    isMoving script now plays footstep sounds (stone/dirt) when moving fast enough

--- Assets/Scripts/IsMoving.cs
+++ Assets/Scripts/IsMoving.cs
@@ -8,8 +8,8 @@ public class IsMoving : MonoBehaviour
 {
     private SoundManager sm;
     private Vector3 lastPos;
-    public float velocitySensetivity = 0.000001f;
-    public float positionSensetivity = 0.01f;
+    // private float velocitySensetivity = 0.000001f;
+    private float positionSensetivity = 0.18f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,21 +25,48 @@ public class IsMoving : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.y - lastPos.y) > 0.1f)
-        {
-            return;
-            // TODO: should also get tag of any ground surface, if you're not touching it, no foot steps get played.
-        }
-        if ((gameObject.GetComponent<Rigidbody>().velocity.x > velocitySensetivity  ||
-             gameObject.GetComponent<Rigidbody>().velocity.z > velocitySensetivity) &&
-            (Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.x - lastPos.x) > positionSensetivity ||
-             Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.z - lastPos.z) > positionSensetivity ))
-        {
-            sm.PlayFootStep();
-        }
         
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        float x_movement = Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.x - lastPos.x);
+        float z_movement = Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.z - lastPos.z);
+        if (x_movement > positionSensetivity) {
+            Debug.Log("x: " + x_movement + " sens: " + positionSensetivity);
+        }
+
+        if (z_movement > positionSensetivity) {
+            Debug.Log("z: " + z_movement + " sens: " + positionSensetivity);
+        }
+
+        if (x_movement > positionSensetivity || z_movement > positionSensetivity)
+        {
+            if (other.collider.tag == "Ground")
+                sm.PlayFootStepsStone();
+            else if (other.collider.tag == "Dirt")
+                sm.PlayFootStepsDirt();
+        }
+        // Debug.Log("Colliding with:" + other.collider.tag + " at speed: " + 
+        //           Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.y - lastPos.y));
+        // if (Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.y - lastPos.y) < 0.0001f)
+        // {
+        //     return;
+        // }
+        // if ((gameObject.GetComponent<Rigidbody>().velocity.x > velocitySensetivity  ||
+        //      gameObject.GetComponent<Rigidbody>().velocity.z > velocitySensetivity) &&
+        //     (Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.x - lastPos.x) > positionSensetivity ||
+        //      Math.Abs(gameObject.GetComponent<Rigidbody>().transform.position.z - lastPos.z) > positionSensetivity ))
+        // {
+        //     if (other.collider.tag == "Ground")
+        //         sm.PlayFootStepsStone();
+        //     else if (other.collider.tag == "Dirt")
+        //         sm.PlayFootStepsDirt();
+        // }
+    
         lastPos = new Vector3(gameObject.GetComponent<Rigidbody>().transform.position.x,
             gameObject.GetComponent<Rigidbody>().transform.position.y,
             gameObject.GetComponent<Rigidbody>().transform.position.z);
+        
     }
 }
