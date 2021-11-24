47cc5b7c24cebc5f9043960eb899687a293ee44b
OwenHW1399 <owenwang1999@outlook.com>
2021-11-23 23:24:34 -0500

    Camera advanced manipulation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,11 +7,18 @@ public class CameraController : MonoBehaviour
 
     public Transform[] followObjects;
     private Vector3 _offset;
-    
+    public float autoZoomStep=0.2f;
+    private float maxViewportDistance = 0.85f;
+    private Vector3 lookDirection;
+    public int zoomLevelBoundaries_max, zoomLevelBoundaries_min;
+    private float zoom_Velocity= 0;
+
+    private Vector3 target_position;
     // Start is called before the first frame update
     void Start()
     {
-        _offset = CalcAveragePos() - transform.position;
+        target_position = CalcAveragePos();
+        _offset = target_position - transform.position;
     }
 
     // returns the average position of every element of followObjects
@@ -28,11 +35,51 @@ public class CameraController : MonoBehaviour
         avg /= followObjects.Length;
         return avg;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private float zoomLevel = 0.0f;
+    // Transformed zoom level
+    private float zoomValue = 1.0f;
+    // Hides transformation of the zoom level into the zoom value
+    // When the zoom level is set, the zoom value is automatically calculated
+    public float ZoomLevel
     {
-        Vector3 avg = CalcAveragePos();
-        transform.position = avg - _offset;
+        get { return zoomLevel; }
+        set
+        {
+            // Recalculate only if zoom level changed
+            if (zoomLevel != value)
+            {
+                zoomLevel = value;
+                // Consider zoom level boundaries and calculate zoom value
+                zoomLevel = Mathf.Clamp(zoomLevel, zoomLevelBoundaries_min, zoomLevelBoundaries_max);
+                zoomValue = Mathf.Pow(1.1f, -zoomLevel);
+            }
+        }
+    }
+    private Vector3 CalcCameraPos()
+    {
+                    Vector2 playerAViewport = Camera.main.WorldToViewportPoint(followObjects[0].position);
+                    Vector2 playerBViewport = Camera.main.WorldToViewportPoint(followObjects[1].position);
+                    float viewportDistance = Vector2.Distance(playerAViewport, playerBViewport);
+                    // If the viewport distance between the player and the enemy is too big, zoom out a little bit
+                    if (viewportDistance > maxViewportDistance)
+                    {
+                        ZoomLevel = Mathf.SmoothDamp(ZoomLevel, ZoomLevel - autoZoomStep, ref zoom_Velocity, Time.deltaTime);
+                    }
+                
+            
+            // Calculate the new camera position. Steps:
+            //  - Position it at the target's position
+            //  - Rotate it towards the target and consider camera tilting
+            //  - Given this rotation move the camera backwards by the zoom value
+            return target_position - Quaternion.LookRotation(new Vector3(lookDirection.x, lookDirection.y, lookDirection.z))
+                *  Vector3.forward * 4.0f * zoomValue;
+        }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = CalcAveragePos();
+        transform.position = CalcCameraPos();
+        transform.rotation = Quaternion.LookRotation(targetPosition - transform.position); // this is not final
     }
 }
