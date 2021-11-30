952701ffa6ea988e6fa2f3412d28e98475458e34
OwenHW1399 <owenwang1999@outlook.com>
2021-11-30 00:42:43 -0500

    Finished lowhigh bridge and the rotating fan

---
+++ Assets/rotate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float spinspeed = 40.0f;
+    
+    public GameObject[] fan_array;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject fan in fan_array)
+        {
+            fan.transform.Rotate(0, spinspeed * Time.deltaTime, 0);
+        }
+    }
+}
