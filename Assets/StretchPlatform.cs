617ce654639c10c4b49845a4199f35565356e281
Kevin Covelli <kevin.covelli@mail.utoronto.ca>
2021-12-05 23:32:25 -0500

    started refactor for hand movement
    downloaded script for searching for component usage in prefabs from https://forum.unity.com/threads/editor-want-to-check-all-prefabs-in-a-project-for-an-attached-monobehaviour.253149/ to fix some dumb errors

--- Assets/StretchPlatform.cs
+++ Assets/StretchPlatform.cs
@@ -8,34 +8,31 @@ public class StretchPlatform : MonoBehaviour
     public Transform player1, player2;
     public Transform platformTarget;
     public Transform cornerNE, cornerSW; // opposite corners, just to get starting dimensions
-    
+
     private float _xStartWidth, _zStartWidth;
 
     private Rigidbody _rb;
-    
+
     private void Start()
     {
         _xStartWidth = Mathf.Abs(cornerNE.position.x - cornerSW.position.x);
         _zStartWidth = Mathf.Abs(cornerNE.position.z - cornerSW.position.z);
         _rb = GetComponent<Rigidbody>();
-
     }
 
     private void FixedUpdate()
     {
-        float targetWidth = Vector3.Distance(platformTarget.position, player1.position) + Vector3.Distance(platformTarget.localPosition,player2.localPosition) ;
+        float targetWidth = Vector3.Distance(platformTarget.position, player1.position) + Vector3.Distance(platformTarget.localPosition, player2.localPosition);
         float targetScale = targetWidth / _xStartWidth;
 
         Vector3 center = Vector3.Lerp(player1.position, player2.position, 0.5f);
-        
+
         transform.localScale = new Vector3(targetScale, 1, 1);
         _rb.MovePosition(center);
-        
     }
 
     public void RaiseCorner(int player, int hand, float t)
     {
-        
+        print("player=" + player + " hand=" + hand + " t="+t);
     }
-    
 }
