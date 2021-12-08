5401fb07915dfca5a594360af8204043c6dad958
Kevin Covelli <kevin.covelli@mail.utoronto.ca>
2021-12-07 19:12:12 -0500

    idk what i changed lol

--- Assets/StretchPlatform.cs
+++ Assets/StretchPlatform.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Assertions.Comparers;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody))]
 public class StretchPlatform : MonoBehaviour
@@ -17,21 +18,19 @@ public class StretchPlatform : MonoBehaviour
     [Header("Transform References")] public Transform player1;
     public Transform player2;
     public Transform platformTarget;
-    public Transform[] corners = new Transform[4];
-
+    [FormerlySerializedAs("corners")] public Transform[] handTargetTransforms = new Transform[4];
+    private float[] handTargetPos = new float[StaticClass.MAXPlayers * 2];// target lerp t between handLocalMin and Max for each corner
+    
     // starting positions / values
     private Vector3[] _cornerOffset = new Vector3[4];
     private float _xStartWidth, _zStartWidth, _startVolume, _handStartY = 0;
     private Vector3 _startScale;
 
-    // target lerp t between handLocalMin and Max for each corner
-    private float[] handTargetPos = new float[StaticClass.MAXPlayers * 2];
-
     private Rigidbody _rb;
 
     private void Start()
     {
-        if (corners.Length != 4)
+        if (handTargetTransforms.Length != 4)
         {
             Debug.LogError("Invalid number of corners");
             return;
@@ -39,8 +38,8 @@ public class StretchPlatform : MonoBehaviour
 
         _rb = GetComponent<Rigidbody>();
 
-        _xStartWidth = Mathf.Abs(corners[0].position.x - corners[3].position.x);
-        _zStartWidth = Mathf.Abs(corners[0].position.z - corners[3].position.z);
+        _xStartWidth = Mathf.Abs(handTargetTransforms[0].position.x - handTargetTransforms[3].position.x);
+        _zStartWidth = Mathf.Abs(handTargetTransforms[0].position.z - handTargetTransforms[3].position.z);
         _startScale = transform.localScale;
         _startVolume = _startScale.x * _startScale.y * _startScale.z;
         if (_startVolume == 0)
@@ -49,11 +48,11 @@ public class StretchPlatform : MonoBehaviour
         _handStartY = 0;
         for (int i = 0; i < 4; i++)
         {
-            _cornerOffset[i] = transform.InverseTransformPoint(corners[i].position);
-            _handStartY += corners[i].localPosition.y;
+            _cornerOffset[i] = transform.InverseTransformPoint(handTargetTransforms[i].position);
+            _handStartY += handTargetTransforms[i].localPosition.y;
         }
 
-        _handStartY /= 4; // corners should always start at same y position, but just in case
+        _handStartY /= 4; // corners should always start at same y position, but average them just in case
     }
 
     private void FixedUpdate()
@@ -69,18 +68,28 @@ public class StretchPlatform : MonoBehaviour
         // _rb.MovePosition(center);
         platformTarget.GetComponent<Rigidbody>().MovePosition(center);
 
-
+        Vector3 avg = Vector3.zero;
         //     _rb.AddForceAtPosition(Vector3.up * cornerForce.Item2 * handStrength, transform.position + cornerForce.Item1, ForceMode.Impulse);
-        for (int i = 0; i < corners.Length; i++)
+        for (int i = 0; i < handTargetTransforms.Length; i++)
         {
-            Vector3 cornerWorldPos = transform.TransformPoint(_cornerOffset[i]); 
-            corners[i].position = cornerWorldPos;
-            corners[i].localPosition += Vector3.up * Mathf.Lerp(handLocalMin, handLocalMax, handTargetPos[i]);
-
-            Vector3 forceDirection = corners[i].position - cornerWorldPos;
+            // reset hand target to actual platform corner
+            Vector3 cornerWorldPos = transform.TransformPoint(_cornerOffset[i]);
+            handTargetTransforms[i].position = cornerWorldPos;
+            
+            // offset hand target based on user input
+            handTargetTransforms[i].localPosition += Vector3.up * Mathf.Lerp(0, handLocalMax, handTargetPos[i]);
+            
+            // clamp hand target to not go too far from player
+            float playerY = i <= 1 ? player1.position.y : player2.position.y;
+            Vector3 clampedWorldPos = new Vector3(handTargetTransforms[i].position.x, Mathf.Clamp(handTargetTransforms[i].position.y, -9999999999999f, playerY + handLocalMax), handTargetTransforms[i].position.z);
+            if(clampedWorldPos != handTargetTransforms[i].position)
+                print($"corner {i} clamped pos");
+            handTargetTransforms[i].position = clampedWorldPos;
+            
+            Vector3 forceDirection = handTargetTransforms[i].position - cornerWorldPos;
             _rb.AddForceAtPosition(forceDirection * handStrength, cornerWorldPos);
-
         }
+        
     }
 
     public void RaiseCorner(int player, int hand, float t)
